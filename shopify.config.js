a7c755998367273251d9861adb6b1e1846a43d3f
sklanier <stevekentlanier@gmail.com>
2022-01-27 11:54:59 -0600

    updated config with shop info

--- shopify.config.js
+++ shopify.config.js
@@ -1,6 +1,6 @@
 export default {
   locale: 'en-us',
-  storeDomain: 'hydrogen-preview.myshopify.com',
+  storeDomain: 'aura-kind.myshopify.com',
   storefrontToken: '5c908b839d8162a9dff61a54682ed38f',
   graphqlApiVersion: 'unstable',
 };
