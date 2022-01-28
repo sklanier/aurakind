e6c3625ab87db0f4e1f96343cae99bb0ddbd9c55
sklanier <stevekentlanier@gmail.com>
2022-01-28 14:00:20 -0600

    changed header

--- src/components/Layout.server.jsx
+++ src/components/Layout.server.jsx
@@ -1,37 +1,17 @@
-import {
-  Image,
-  useShopQuery,
-  flattenConnection,
-  LocalizationProvider,
-} from '@shopify/hydrogen';
+// The `useShopQuery` hook makes server-only GraphQL queries to the Storefront API.
+import {useShopQuery, Link} from '@shopify/hydrogen';
+// Import `gql` to parse GraphQL queries.
 import gql from 'graphql-tag';
-
-import Header from './Header.client';
-import Footer from './Footer.server';
-import {useCartUI} from './CartUIProvider.client';
-import Cart from './Cart.client';
-
-/**
- * A server component that defines a structure and organization of a page that can be used in different parts of the Hydrogen app
- */
-export default function Layout({children, hero}) {
+// The `Layout` component accepts `children` as a prop. This injects any nested components into the layout when you call it.
+export default function Layout({children}) {
+  // Your shop domain and token are automatically configured, so you only need to pass in a GraphQL query.
   const {data} = useShopQuery({
     query: QUERY,
-    variables: {
-      numCollections: 3,
-    },
-    cache: {
-      maxAge: 60,
-      staleWhileRevalidate: 60 * 10,
-    },
   });
-  const {isCartOpen, closeCart} = useCartUI();
-  const collections = data ? flattenConnection(data.collections) : null;
-  const products = data ? flattenConnection(data.products) : null;
-  const storeName = data ? data.shop.name : '';
 
+  // Return JSX with the Tailwind classes that determine the layout styling.
   return (
-    <LocalizationProvider>
+    <div>
       <div className="absolute top-0 left-0">
         <a
           href="#mainContent"
@@ -40,55 +20,22 @@ export default function Layout({children, hero}) {
           Skip to content
         </a>
       </div>
-      <div className="min-h-screen max-w-screen text-gray-700 font-sans">
-        <Header collections={collections} storeName={storeName} />
-        <div>
-          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-          <div
-            className={`z-50 fixed top-0 bottom-0 left-0 right-0 bg-black transition-opacity duration-400 ${
-              isCartOpen ? 'opacity-20' : 'opacity-0 pointer-events-none'
-            }`}
-            onClick={isCartOpen ? closeCart : null}
-          />
-          <Cart />
-        </div>
-        <main role="main" id="mainContent" className="relative bg-gray-50">
-          {hero}
-          <div className="mx-auto max-w-7xl p-4 md:py-5 md:px-8">
-            {children}
-          </div>
-        </main>
-        <Footer collection={collections[0]} product={products[0]} />
-      </div>
-    </LocalizationProvider>
+      <header className="my-10 text-center">
+        <h1 className="font-bold uppercase tracking-wider text-2x-l">
+          <Link to="/">{data.shop.name}</Link>
+        </h1>
+      </header>
+      <main id="mainContent" className="mx-auto max-w-7xl p-4 md:py-5 md:px-8">
+        {children}
+      </main>
+    </div>
   );
 }
-
+// The GraphQL query used to fetch shop data.
 const QUERY = gql`
-  query indexContent($numCollections: Int!) {
+  query ShopNameQuery {
     shop {
       name
     }
-    collections(first: $numCollections) {
-      edges {
-        node {
-          description
-          handle
-          id
-          title
-          image {
-            ...ImageFragment
-          }
-        }
-      }
-    }
-    products(first: 1) {
-      edges {
-        node {
-          handle
-        }
-      }
-    }
   }
-  ${Image.Fragment}
 `;
